b695e7d8566a1f8152f915492cb7846a0413a6f3
Samuel C <vanctardi@protonmail.com>
2023-01-28 19:11:58 -0500

    Pre-dinner commit, code is not fully working

--- db.py
+++ db.py
@@ -8,9 +8,9 @@ def openDb():
     '''
     con = sqlite3.connect("workout.db")
     cur = con.cursor()
-    if cur.fetchall() == False:
+    if cur.fetchall() == []:
         cur.execute("CREATE TABLE workout(date, type, reps, weight)") 
-        cur.execute("CREATE TABLE heatmap(group, relative_num")
+    # cur.execute("CREATE TABLE heatmap(group, relative_num)")
         # If you have 3 sets just call add 3 lines so they can all have diff. 
         # weight and reps
     return con
@@ -18,31 +18,33 @@ def openDb():
 def retrieveRows():
     pass
 
-def addRows(array, tableNum, db):
+def addRows(data, tableNum, db):
     '''
     Adds row(s) to the database.
-        Parameters: array (array): List containing the rows to add.
-                    tableNum (int): Table number
+        Parameters: data (array): List containing the rows to add. Each row should be a tuble, within a list
+                    tableName (int): Table num: 0 for tracker, 1 for heatmap
                     db: the database to edit
         Returns: database object (sqlite)
     '''
-    # cur.execute("""
-    """INSERT INTO movie VALUES
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)"""
-    pass
+    cur = db.cursor()
+    # TODThis will definitely break if not handled properly, add a check when you've got time
+    if tableNum == 0: 
+        res = cur.execute("SELECT name FROM sqlite_master")
+        print(res.fetchone())
+        cur.executemany("INSERT INTO workout VALUES(?, ?, ?, ?)", data)
+    elif tableNum == 1: cur.executemany("INSERT INTO heatmap VALUES(?, ?, ?)", data)
+    db.commit()
     
 
 def deleteRows():
     pass
 
-def debugPrintTable(db):
-    '''
-    Adds row(s) to the database.
-        Parameters: rowArray (array): Numpy array containing the rows to add.
-                    tableNum (int): Table number
-        Returns: database object (sqlite)
-    '''
+# def debugPrintTable(db):
+#     '''
+#     Prints the entire contents of the database for a given table.
+#     Parameters: tableNum (int): Table num: 0 for tracker, 1 for heatmap
+#     '''
+
 
 def closeDb(db):
     '''
@@ -51,7 +53,11 @@ def closeDb(db):
         Returns: status (bool): 0 if closed correctly
     '''
     db.close()
+    db = None
     return 0
 
 workout = openDb();
-addRows([20230128,"Chest Press",8,135.0])
+input()
+addRows(data = [(20230128,"Chest Press",8,135.0)], tableNum=0, db=workout)
+closeDb(workout)
+input()
