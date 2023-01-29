697346fa98bd4404d976363449b5efcd02fcdde5
Samuel C <vanctardi@protonmail.com>
2023-01-29 08:35:11 -0500

    Moving around, implemented database connection

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request, url_for,  redirect, abort, flash
 import sqlite3
+import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-def get_db_connection():
-    conn = None
-    conn = sqlite3.connect('workouts.db')
+def get_db_connection(): # Untested
+    conn = db.openDb()
+    # conn = None
+    # conn = sqlite3.connect('workouts.db')
     conn.row_factory = sqlite3.Row
     return conn
-def get_workout(post_id):
+
+def get_workout(post_id): # Untested, to be fixed
     conn = get_db_connection()
     workouts = conn.execute('SELECT * FROM workouts WHERE id = ?',
                         (post_id)).fetchone()
-    conn.close()
+    db.closeDb(conn)
     if workouts is None:
         abort(404)
     return workouts
+
 @app.route('/')
 def start():
     return render_template('home.html')
@@ -35,8 +39,6 @@ def progress():
 @app.route('/input')
 def input():
     return render_template('input.html')
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
