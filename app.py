00db0128f3f93ea5f3698e72841f92be7a6218d6
Samuel C <vanctardi@protonmail.com>
2023-01-29 08:50:33 -0500

    L

--- app.py
+++ app.py
@@ -22,23 +22,64 @@ def get_workout(post_id): # Untested, to be fixed
     return workouts
 
 @app.route('/')
-def start():
-    return render_template('home.html')
-@app.route('/home')
-def home():
-    return render_template('home.html')
-@app.route('/muscle')
-def muscle():
-    return render_template('muscle.html')
-@app.route('/data')
-def data():
-    return render_template('data.html')
-@app.route('/progress')
-def progress():
-    return render_template('progress.html')
-@app.route('/input')
-def input():
-    return render_template('input.html')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+
+def edit(id):
+    post = get_post(id)
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+
+        elif not content:
+            flash('Content is required!')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+@app.route('/<int:id>/delete/', methods=('POST',))
+
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
