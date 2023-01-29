897edffeac5edef6a480ebc2e65a113c19691037
Z3R0z <79765884+Z3R0z@users.noreply.github.com>
2023-01-29 07:24:07 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, url_for,  redirect, abort, flash
+import sqlite3
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+def get_db_connection():
+    conn = None
+    conn = sqlite3.connect('workouts.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+def get_workout(post_id):
+    conn = get_db_connection()
+    workouts = conn.execute('SELECT * FROM workouts WHERE id = ?',
+                        (post_id)).fetchone()
+    conn.close()
+    if workouts is None:
+        abort(404)
+    return workouts
+@app.route('/')
+def start():
+    return render_template('home.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+@app.route('/muscle')
+def muscle():
+    return render_template('muscle.html')
+@app.route('/data')
+def data():
+    return render_template('data.html')
+@app.route('/progress')
+def progress():
+    return render_template('progress.html')
+@app.route('/input')
+def input():
+    return render_template('input.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,83 @@
+import sqlite3
+
+con = sqlite3.connect("workouts.db")
+cur = con.cursor()
+cur = cur.execute("CREATE TABLE IF NOT EXISTS workouts(date INTEGER NOT NULL, type TEXT NOT NULL, \
+        reps INTEGER NOT NULL, weight REAL NOT NULL)")
+cur = cur.execute("CREATE TABLE IF NOT EXISTS heatmap(date INTEGER NOT NULL, grp TEXT NOT NULL, \
+    score INTEGER NOT NULL)") #, UNIQUE (date, grp))
+
+
+def addRows(data, tableNum, db):
+    '''
+    Adds row(s) to the database.
+        Parameters: 
+            data (array): List containing the rows to add. Each row should be a tuble, within a list
+                tableName (int): Table num: 0 for tracker, 1 for heatmap
+            db: the database to edit
+        Returns: database object (sqlite)
+    '''
+    cur = db.cursor()
+    if tableNum == 0: cur.executemany("INSERT INTO workouts VALUES(?, ?, ?, ?)", data)
+    elif tableNum == 1: cur.executemany("INSERT INTO heatmap VALUES(?, ?, ?)", data)
+    else: raise Exception("Incorrect tableNum.")
+    db.commit()
+    
+
+def __searchOptions(tableNum, date, other_query):
+    '''
+    Internal function for getting search options.
+        Parameters:
+            tableName (int): Table num: 0 for tracker, 1 for heatmap
+            date (int/None): the date. Can be either an int or None
+            other_query: (string/None): either muscle group (grp) for heatmap or type for the exercise
+    '''
+    options = ""
+    if tableNum != 0 and tableNum != 1: raise Exception("Incorrect tableNum.")
+    if tableNum == 0 and other_query != None: options += " type = \"" + str(other_query) + "\""
+    if tableNum == 1 and other_query != None: options += " grp = \"" + str(other_query) + "\""
+    if date != None: options += " date = " + str(date)
+    if options != "": options = " WHERE" + options
+    return options
+
+def deleteRows(db, tableNum, date, other_query):
+    '''
+    Deletes rows that match the search parameters.
+    '''
+    cur = db.cursor()
+    options = __searchOptions(tableNum, date, other_query)
+    if options != None or options != "":
+        if tableNum == 0: cur.execute("DELETE FROM workout" + options)
+        if tableNum == 1: cur.execute("DELETE FROM heatmap" + options)
+
+def fetchRows(db, tableNum, date, other_query):
+    '''
+    Returns the contents of the database for a given table for filtered rows.
+    Parameters: tableNum (int): Table num: 0 for tracker, 1 for heatmap
+    ''' # TODO: add all exercises mode?
+    cur = db.cursor()
+    searchOptions = __searchOptions(tableNum, date, other_query)
+
+    if tableNum == 0: return cur.execute("SELECT * FROM workout" + searchOptions).fetchall()
+    elif tableNum == 1: return cur.execute("SELECT * FROM heatmap" + searchOptions).fetchall()
+    else: raise Exception("Incorrect tableNum.")
+
+def debugPrintTable(db, tableNum):
+    '''
+    Prints the entire contents of the database for a given table.
+    Parameters: tableNum (int): Table num: 0 for tracker, 1 for heatmap
+    '''
+    cur = db.cursor()
+    if tableNum == 0: 
+        for row in cur.execute("SELECT * FROM workout ORDER BY date").fetchall(): print(row)
+    elif tableNum == 1:
+        for row in cur.execute("SELECT * FROM heatmap ORDER BY date").fetchall(): print(row)
+    else: raise Exception("Incorrect tableNum.")
+
+def closeDb(db):
+    '''
+    Closes the database.
+        Parameters: database object: database to close
+    '''
+    db.close()
+    db = None
